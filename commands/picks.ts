6018dfab48c80ab37ab00fbdd3b719dab5cb57ff
Maize~ <68474964+ImmortalMaize@users.noreply.github.com>
2022-09-25 21:51:45 -0400

    added yearly

--- commands/picks.ts
+++ commands/picks.ts
@@ -16,13 +16,18 @@ export default new ReadableCommand(
     .addSubcommand(
         subcommand => subcommand
         .setName("monthly")
-        .setDescription("Get finished picks for this week.")
+        .setDescription("Get finished picks for this month.")
+    )
+    .addSubcommand(
+        subcommand => subcommand
+        .setName("yearly")
+        .setDescription("Get finished picks for this year.")
     )
     , async (interaction: ChatInputCommandInteraction) => {
         const finishedPicks = (await interaction.guild.channels.fetch()).get(channels["finished-beeps"]) as TextChannel
-
         const scope = interaction.options.getSubcommand()
-        const backWhen = scope === "monthly" ? utils.goBack(1, "month") : utils.goBack(1, "week")
+
+        const backWhen = scope === "yearly" ? utils.goBack(1, "year") : scope === "monthly" ? utils.goBack(1, "month") : utils.goBack(1, "week") 
         const picks = (await finishedPicks.messages.fetch()).filter(message => message.createdTimestamp > backWhen).map(pick => pick)
         const pickReactions = picks.map(pick => pick.reactions.cache.get(utils.hand).count).sort((a, b) => b-a)
 
