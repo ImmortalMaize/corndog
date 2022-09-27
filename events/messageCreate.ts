755ce46fcb9f4f2e90163a8f1d76bac57d322631
Maize~ <68474964+ImmortalMaize@users.noreply.github.com>
2022-09-27 08:05:44 -0400

    yeah

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -9,6 +9,7 @@ const reply = async (message: Message, content: string) => {
         await reply.delete()
     }, 8000)
 }
+
 export default new ReadableEvent("messageCreate", async (message: Message) => {
     if (message.author.id === config.clientId) {
         return
@@ -22,17 +23,17 @@ export default new ReadableEvent("messageCreate", async (message: Message) => {
         if (message.cleanContent.length > 450) {
             console.log("Message length is bad.")
             bad = true
-            reply(message, "The character limit is 450 or under. Yours is " + message.cleanContent.length + "! > _<")
+            await reply(message, "The character limit is 450 or under. Yours is " + message.cleanContent.length + "! > _<")
         }
         if (message.cleanContent.match(/\n/gm)?.length >= 5) {
             console.log("Message line breaks are bad.")
             bad = true
-            reply(message, "Too many line breaks! > _<")
+            await reply(message, "Too many line breaks! > _<")
         }
         if (!message.cleanContent.match(utils.hasSauce)) {
             console.log("No link = bad!")
             bad = true
-            reply(message, "Where's the link?! > _< (Make sure it starts with https://)")
+            await reply(message, "Where's the link?! > _< (Make sure it starts with https://)")
         } else {
             console.log("There's a link... Okay.")
             if (!message.cleanContent.match(utils.hasSauce)?.every(
@@ -40,7 +41,7 @@ export default new ReadableEvent("messageCreate", async (message: Message) => {
             )) {
                 console.log("The link is too long. BAD.")
                 bad = true
-                reply(message, "Shorten your link(s)...! > _<")
+                await reply(message, "Shorten your link(s)...! > _<")
             }
         }
 
