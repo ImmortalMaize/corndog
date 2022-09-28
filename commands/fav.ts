0c5c8de13124b35d607c39bbe8c001b3578515fc
Maize~ <68474964+ImmortalMaize@users.noreply.github.com>
2022-09-28 10:04:15 -0400

    new modal

---
+++ commands/fav.ts
@@ -0,0 +1,29 @@
+import { ReadableCommand } from "../classes";
+import { ActionRowBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle, Interaction } from 'discord.js';
+
+export default new ReadableCommand(
+    new SlashCommandBuilder().setName("fav").setDescription("Set a bookmark."),
+    async (interaction: Interaction) => {
+        const favModal = new ModalBuilder()
+        .setCustomId('fav')
+        .setTitle("Set Bookmark")
+
+        const sauceInput = new TextInputBuilder()
+        .setCustomId("sauce")
+        .setLabel("What's the sauce?")
+        .setStyle(TextInputStyle.Short)
+
+        const descInput = new TextInputBuilder()
+        .setCustomId("desc")
+        .setLabel("What's the description?")
+        .setStyle(TextInputStyle.Short)
+
+        const row1 = new ActionRowBuilder().addComponents(sauceInput)
+        const row2 = new ActionRowBuilder().addComponents(descInput)
+
+        //@ts-ignore
+        favModal.addComponents(row1, row2)
+        //@ts-ignore
+        await interaction.showModal(favModal)
+    }
+)
