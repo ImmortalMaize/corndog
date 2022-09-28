8657eca51d366fa700b8adee8c0e4a74c3128e6e
Maize~ <68474964+ImmortalMaize@users.noreply.github.com>
2022-09-28 14:22:28 -0400

    changed fav command

--- commands/fav.ts
+++ commands/fav.ts
@@ -1,29 +1,27 @@
 import { ReadableCommand } from "../classes";
-import { ActionRowBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle} from 'discord.js';
+import { ActionRowBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle, ChatInputCommandInteraction } from 'discord.js';
 
 export default new ReadableCommand(
-    new SlashCommandBuilder().setName("fav").setDescription("Set a bookmark."),
-    async (interaction) => {
-        const favModal = new ModalBuilder()
-        .setCustomId('fav')
-        .setTitle("Set Bookmark")
+    new SlashCommandBuilder().setName("fav")
+    .setDescription("Set a bookmark.")
+    .addStringOption(
+        option => option
+        .setName("sauce")
+        .setDescription("What's the sauce?")
+        .setRequired(true)
+    )
+    .addStringOption(
+        option => option.setName("name")
+        .setDescription("What's the name?")
+        .setRequired(true)
+    ),
+    async (interaction: ChatInputCommandInteraction) => {
+        const sauce = interaction.options.getString("sauce")
+        const name = interaction.options.getString("name")
 
-        const sauceInput = new TextInputBuilder()
-        .setCustomId("sauce")
-        .setLabel("What's the sauce?")
-        .setStyle(TextInputStyle.Short)
-
-        const descInput = new TextInputBuilder()
-        .setCustomId("desc")
-        .setLabel("What's the description?")
-        .setStyle(TextInputStyle.Short)
-
-        const row1 = new ActionRowBuilder().addComponents(sauceInput)
-        const row2 = new ActionRowBuilder().addComponents(descInput)
-
-        //@ts-ignore
-        favModal.addComponents(row1, row2)
-        //@ts-ignore
-        await interaction.showModal(favModal)
+        interaction.reply({
+            content: name + ", " + sauce,
+            ephemeral: true
+        })
     }
 )
--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -4,8 +4,6 @@ import { ReadableEvent } from "../classes"
 export default new ReadableEvent('interactionCreate', async (interaction: Interaction) => {
         // @ts-ignore
         const command = interaction.client.commands.get(interaction.commandName);
-        console.log(interaction.isModalSubmit())
-        console.log(interaction.isChatInputCommand())
     if (interaction.isChatInputCommand()) {
         try {
             await command.execute(interaction);
@@ -14,17 +12,4 @@ export default new ReadableEvent('interactionCreate', async (interaction: Intera
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     }
-
-    if (interaction.isModalSubmit()) {
-        const modalInteraction = interaction as ModalSubmitInteraction
-        console.log(interaction)
-
-        const sauce = modalInteraction.fields.getTextInputValue("sauce")
-        const description = modalInteraction.fields.getTextInputValue("desc")
-
-        modalInteraction.reply({
-            ephemeral: true,
-            content: sauce + ", " + description
-        })
-    }
-    })
+})
