977bfdd6d0dd1b26caa365d60b8a638701d59d99
Maize~ <68474964+ImmortalMaize@users.noreply.github.com>
2022-09-28 22:20:10 -0400

    oe

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -4,7 +4,7 @@ import { ReadableEvent } from "../classes"
 export default new ReadableEvent('interactionCreate', async (interaction: Interaction) => {
         // @ts-ignore
         const command = interaction.client.commands.get(interaction.commandName);
-    if (interaction.isChatInputCommand()) {
+    if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
         try {
             await command.execute(interaction);
         } catch (error) {
