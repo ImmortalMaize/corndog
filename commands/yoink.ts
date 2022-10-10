f1388b95fcc8bdb152940d32d624898e6645bbb4
Maize~ <68474964+ImmortalMaize@users.noreply.github.com>
2022-10-10 09:24:59 -0400

    maize

--- commands/yoink.ts
+++ commands/yoink.ts
@@ -1,5 +1,5 @@
 import ReadableCommand from "../classes/ReadableCommand";
-import { SlashCommandBuilder, ChatInputCommandInteraction, InteractionType } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, InteractionType, InteractionResponse } from 'discord.js';
 import { roles } from "../config";
 import { timeControl } from "../redis/entities";
 import utils from "../utils";
@@ -7,23 +7,32 @@ import { userMention } from 'discord.js';
 
 export default new ReadableCommand(new SlashCommandBuilder().setName("yoink").setDescription("Yoinks! >:3c"), async (interaction: ChatInputCommandInteraction) => {
     const check = await timeControl.check("yoink", undefined, true)
-
     if (check) {
-        let replyContent: string
         const member = (await interaction.guild.members.fetch()).get(interaction.user.id);
         const role = roles["some role idk"]
         
+        let reply: InteractionResponse
+        
         try {
         interaction.guild.members.fetch()
-        .then(members => members.each(member => {
-            if (member.roles.cache.has(role)) member.roles.remove(role)
-        }))
-        .then(() => member.roles.add(role))
-
-        const reply = await interaction.reply({
-            content: `${userMention(member.id)} yoinked it!`,
-            ephemeral: false
-        })
+        .then(
+            members => members.each(member => {
+                if (member.roles.cache.has(role)) member.roles.remove(role)
+            })
+        )
+        .catch(
+            () => interaction.reply("It's unyoinkable...")
+        )
+        .then(async () => await member.roles.add(role))
+        .catch(
+            () => interaction.reply("You can't yoink it! >:(")
+        )
+        .then(
+            async () => await interaction.reply({
+                content: `${userMention(member.id)} yoinked it!`,
+                ephemeral: false
+            }
+        ))
 
         //@ts-ignore
         await timeControl.generate({
