d93259eeac2814d39f08bea93a95149f54479882
Maize~ <the.scarlet.armada@gmail.com>
2023-05-11 01:12:45 -0400

    idk

--- commands/dig.ts
+++ commands/dig.ts
@@ -55,7 +55,7 @@ Lying there, at the bottom of the hole you've dug, is a small sliver of paper, w
         timeControl.generate({
             channel: interaction.channelId,
             message: "",
-            cooldown: utils.time.goForth(1, "hours").toDate(),
+            cooldown: utils.time.goForth(1, "minute").toDate(),
             name: "dig_" + interaction.user.id,
         })
     }
