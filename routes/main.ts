aee3281b5d9e6f52f7a90a5d5502f23e83b37480
Maize~ <the.scarlet.armada@gmail.com>
2023-12-26 10:35:58 -0500

    fixed html

--- routes/main.ts
+++ routes/main.ts
@@ -1,11 +1,9 @@
 import ReadableRoute from "../classes/ReadableRoute";
-import { main } from "./html";
 
 export default new ReadableRoute(
     '/', {
         get(_request, response) {
-            console.log(main)
             response.setHeader('Content-Type', 'text/html');
-            response.send(main)
+            response.sendFile('./html/main.html')
         }
     })
