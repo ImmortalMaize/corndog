6f81943dd92ad2cfa3bd957075765bc2249bbadc
Maize~ <the.scarlet.armada@gmail.com>
2025-01-12 19:24:27 -0500

    changed something

--- events/messageReactionAdd/onFlag.ts
+++ events/messageReactionAdd/onFlag.ts
@@ -1,17 +1,19 @@
 import { Message, TextChannel } from "discord.js"
 import { channels } from "../../config"
-import { getChannel, getReactions, emojis, woof } from "../../utils"
+import { getChannel, getReactions, emojis, woof, tracer } from "../../utils"
 import generateFlagMessage from "./generateFlagMessage"
 import { report as reportInventory } from "../../redis/entities"
+import { inverse } from "colors"
 
 export default async (message: Message) => {
     const { url, cleanContent, member, guild, attachments } = message
-    console.log(member.user.id)
+    tracer.log(`Message by ${inverse(member.user.username)} reported.`)
     const reportsChannel = await getChannel(guild.channels, channels.reports) as TextChannel
-    const reports = (await getReactions(message, emojis.report).users.fetch()).filter(user => user.id !== "203221713440210944").size
+    const reports = (await getReactions(message, emojis.report).users.fetch()).size
     const existingReport = await reportInventory.get("link", url)
 
     if (existingReport) {
+        tracer.info("Report already generated for " + existingReport.link)
         const { link, mod, content } = existingReport
         const reportMessage = await reportsChannel.messages.fetch(existingReport.id)
 
@@ -23,6 +25,7 @@ export default async (message: Message) => {
     if (reports >= 3) {
         const attachmentLinks = attachments.map(attachment => attachment.url).join(' ')
         const content = cleanContent + " " + attachmentLinks
+        console.log("yeah")
         if (message.channelId !== channels["finished-beeps"]) message.reply("I have reported this message to management. Someone should be here shortly. " + woof() + "!") 
         
             const report = await reportsChannel.send(generateFlagMessage(url, content, member.user))
