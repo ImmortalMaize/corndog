6cf72ca06ffe1ceb04e4f9d36af6b2d00dd3bf92
Maize~ <the.scarlet.armada@gmail.com>
2025-03-28 23:14:30 -0400

    fixed URL regex again

--- utils/hasUrl.ts
+++ utils/hasUrl.ts
@@ -1,1 +1,1 @@
-export const hasUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm
+export const hasUrl = /(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/gm
