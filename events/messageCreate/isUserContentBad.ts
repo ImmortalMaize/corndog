0ccd217af5171d8f68c1165b4abad536eb237ba7
Maize~ <the.scarlet.armada@gmail.com>
2025-04-19 08:49:37 -0400

    no application files allowed

--- events/messageCreate/isUserContentBad.ts
+++ events/messageCreate/isUserContentBad.ts
@@ -4,6 +4,9 @@ import { hasUrl } from "../../utils"
 export default async function isUserContentBad(message: Message): Promise<boolean> {
     const { attachments, cleanContent } = message
     const noAttachments = attachments.size === 0
+    const applicationAttachment = noAttachments ? false : attachments.first().contentType.match(/^application/g) ? true : false
+    if (applicationAttachment) return true
+    
     const urls = cleanContent.match(hasUrl)
     return !urls && noAttachments ? true : false
 }
