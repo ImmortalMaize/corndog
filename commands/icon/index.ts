68ed8524298cc0ab849d7b65ccc4a35a33dfda79
Maize~ <the.scarlet.armada@gmail.com>
2025-04-29 13:08:09 -0400

    role icons

---
+++ commands/icon/index.ts
@@ -0,0 +1,32 @@
+import { ChatInputCommandInteraction, GuildMember, SlashCommandBuilder } from "discord.js";
+import { ReadableCommand } from "../../classes";
+import iconMap from "./map"
+import { emote, woof } from "../../utils";
+
+
+async function setIconRole(member: GuildMember, icon: string) {
+    const iconTuple = iconMap.get(icon)
+    const [requiredRole, iconRole] = iconTuple
+    const iconRoles = Array.from(iconMap.values()).map(tuple => tuple[1])
+
+    if (!icon) await member.roles.remove(iconRoles)
+    if (!member.roles.cache.has(requiredRole)) return false
+    
+    await member.roles.remove(iconRoles).then(async () => await member.roles.add(iconRole))
+    return true
+}
+
+export default new ReadableCommand(new SlashCommandBuilder().setName("icon").setDescription("Set your icon!").addStringOption(option => option.setName("role").setDescription("What role do you want an icon for?").setRequired(true).setChoices(
+    { name: "Hypergeek", value: "hypergeek" },
+    { name: "Heckin' Typing Maniac", value: "maniac" },
+    { name: "BeepBoxer Of High Society", value: "society" },
+    { name: "No Icon", value: "nothing" })), async (interaction: ChatInputCommandInteraction) => {
+        const { member } = interaction
+        const role = interaction.options.getString("role")
+        const success = await setIconRole(member as GuildMember, role === "nothing" ? null : role)
+
+        interaction.reply({
+            content: success ? `${woof()}! Icon set! ${emote("elated")}` : `${woof()}.. you don't have the needed role for that icon. ${emote("malcontent")}`,
+            ephemeral: true
+        })
+    })
--- events/messageCreate/isUserContentBad.ts
+++ events/messageCreate/isUserContentBad.ts
@@ -5,6 +5,7 @@ export default async function isUserContentBad(message: Message): Promise<boolea
     const { attachments, cleanContent } = message
     const noAttachments = attachments.size === 0
     const tooManyAttachments = attachments.size > 1
+    attachments.first()
     const applicationAttachment = noAttachments ? false : attachments.first().contentType.match(/^application/g) ? true : false
     if (tooManyAttachments || applicationAttachment) return true
     
