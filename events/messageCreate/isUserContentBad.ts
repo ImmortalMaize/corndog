f470d64ae4ee083c44a530e414b95c7db494bb67
Maize~ <the.scarlet.armada@gmail.com>
2025-09-27 17:05:52 -0400

    more images

--- events/messageCreate/isUserContentBad.ts
+++ events/messageCreate/isUserContentBad.ts
@@ -4,10 +4,10 @@ import { hasUrl } from "../../utils"
 export default async function isUserContentBad(message: Message): Promise<boolean> {
     const { attachments, cleanContent } = message
     const noAttachments = attachments.size === 0
-    const tooManyAttachments = attachments.size > 1
+    const tooManyAttachments = attachments.filter(attachment => !attachment.contentType.match(/^image/g)).size > 1
     attachments.first()
     const applicationAttachment = noAttachments ? false : attachments.first().contentType.match(/^application/g) ? true : false
-    if (tooManyAttachments || applicationAttachment) return true
+    if (tooManyAttachments|| applicationAttachment) return true
     
     const urls = cleanContent.match(hasUrl)
     return !urls && noAttachments ? true : false
